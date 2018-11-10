0a0c253859f59481253b46ea038ba1fa062ccc5f
Uladzimir Sapeshka <usapeshka@gmail.com>
2018-11-10 16:28:17 -0600

    KMeans computeMeans member function added

--- KMeans.cpp
+++ KMeans.cpp
@@ -19,14 +19,14 @@ class KMeans
 public:
 	KMeans(){};
         ~KMeans(){};
+
 	void clusterData(valarray<pair<double,double>>& init_means_, int num_iters_){
 		m_means = init_means_;
 		this->assignLabels();
 
 		int i = 0;
-		while (i < num_iters_) {
+		while (i < num_iters_ && !this->computeMeans()) {
 			cout << "Running iteration: " << i << "\r\n";
-			this->computeMeans();
 			this->assignLabels();
 			i++;
 		}
@@ -34,44 +34,42 @@ public:
 	}
 
 	void printClusters() const {
+		for (int k = 0; k < m_k; k++) {
+			for(const auto & it : m_data[k]){
+				cout << " [" << get<0>(it) <<" , " << get<1>(it) << "]";
+			}
+			cout << "\n";
+		}
 
 	}
 
 private:
-	bool computeMeans() {
+	bool computeMeans(){
 		bool res = true;
-		for (int i = 0; i < m_k; i++) {
+		for(int i = 0; i < m_k ; i++ ) {
 			pair<double,double> mean(0,0);
-			int num_features_of_k = m_data[k].size();
-			for (auto const & it : m_data[k]) {
+			int num_features_of_k = m_data[i].size();
+			for(auto const & it: m_data[i]) {
 				get<0>(mean) += get<0>(it);
 				get<1>(mean) += get<1>(it);
 			}
 			get<0>(mean) /= num_features_of_k;
 			get<1>(mean) /= num_features_of_k;
-			// check for conv
-			res = (m_means[k] == means && res == true) ? true: false;
-			m_means[k] = mean;
-			cout << "Cluster centroid << "<<k <<":\tx"<<get<0>(mean)<<
-				","<< get<1>(mean)<<"\t";
-		} 
-		cout << "\r\n";
+			res = (m_means[i] == mean && res == true) ? true : false;
+			m_means[i] = mean;
+			cout << " Cluster Centroid << " << i << ":\tx" << get<0>(mean) << " , y " << get<1>(mean) << "\t";
+
+		}
+		cout <<"\r\n";
 		return res;
-
 	}
 
-	void assignLabels() {
-
-	}
-	
-	int computeClosestCentroid() {
-
-	}
-	// data members
+	// Private data members
 	int m_k;
-	int m_features;
+	itn m_features;
 	valarray<pair<double,double>> m_means;
-	valarray< vector<pair<double,double> > > m_data;
+	valarray<vector<pair<double,double>>> m_data;
+	
 
 };
 int main(int argc, const char *argv[])
